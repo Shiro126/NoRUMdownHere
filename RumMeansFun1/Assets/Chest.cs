a38ac398f59e45d39e3657b9b7612db503641454
Juk3n <34384360+Juk3n@users.noreply.github.com>
2017-12-20 02:06:05 +0100

    Prototyp v1.0

---
+++ RumMeansFun1/Assets/Chest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour {
+
+    public Sprite[] Sprites;
+    public int currentSprite;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            currentSprite++;
+            currentSprite %= Sprites.Length;
+
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Sprites[currentSprite];
+        }
+    }
+}
---
+++ RumMeansFun1/Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+    public float movementSpeed = 10f;
+
+    float axisY = 0f;
+    float axisX = 0f;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        axisY = Input.GetAxis("Vertical") * movementSpeed;
+        axisX = Input.GetAxis("Horizontal") * movementSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.y = axisY;
+        velocity.x = axisX;
+        rb.velocity = velocity;
+    }
+}
